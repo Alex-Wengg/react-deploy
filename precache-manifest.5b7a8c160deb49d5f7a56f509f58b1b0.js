b6981f2f53e431c539bc90614fa95e279401ffb3
Han-Weng <hanweng9@gmail.com>
2021-06-14 19:07:52 -0400

    Updates

---
+++ precache-manifest.5b7a8c160deb49d5f7a56f509f58b1b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744cabb10c3ecf4dce396ed8cbf918bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a64e2764ad419017b0",
+    "url": "/static/css/main.685824d5.chunk.css"
+  },
+  {
+    "revision": "fdd678464e2ee1e0c5a4",
+    "url": "/static/js/2.0a89b01d.chunk.js"
+  },
+  {
+    "revision": "00a64e2764ad419017b0",
+    "url": "/static/js/main.8062eae6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
